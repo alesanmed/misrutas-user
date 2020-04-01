db2b52e84c98479ad226fbd5ae8fa9a1c9b182b3
Ale Sánchez <alesanchezmedina@gmail.com>
2020-04-01 17:47:57 +0200

    Decreased the timeout to 5 seconds

--- src/interceptors/TimeoutInterceptor.ts
+++ src/interceptors/TimeoutInterceptor.ts
@@ -7,7 +7,7 @@ export class TimeoutInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     Logger.log(`URL called: ${context.switchToHttp().getRequest().method} ${context.switchToHttp().getRequest().originalUrl}`);
     return next.handle().pipe(
-      timeout(50000),
+      timeout(5000),
       catchError(err => {
         if (err instanceof TimeoutError) {
           return throwError(new RequestTimeoutException());
