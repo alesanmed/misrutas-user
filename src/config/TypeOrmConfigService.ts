3a90f532ca7effbd608c8f030882f1a861ade8e1
Ale Sánchez <alesanchezmedina@gmail.com>
2020-05-18 16:55:27 +0200

    Changed names for avoiding collisions

--- src/config/TypeOrmConfigService.ts
+++ src/config/TypeOrmConfigService.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { User } from 'src/user/user.entity';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
@@ -8,10 +8,13 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory{
   constructor(private configService: ConfigService) { }
   
   createTypeOrmOptions(): TypeOrmModuleOptions {
+    Logger.log(`Port: ${this.configService.get<number>('POSTGRES_PORT')}`)
+    Logger.log(this.configService.get('POSTGRES_HOST'))
+    Logger.log(this.configService.get('POSTGRES_PORT'))
     return {
       type: 'postgres',
-      host: this.configService.get<string>('POSTGRES_HOST') || '',
-      port: this.configService.get<number>('POSTGRES_PORT') || 1,
+      host: this.configService.get<string>('POSTGRES_DB_HOST') || '',
+      port: this.configService.get<number>('POSTGRES_DB_PORT') || 1,
       username: this.configService.get<string>('POSTGRES_USERNAME') || '',
       password: this.configService.get<string>('POSTGRES_PASSWORD') || '',
       database: this.configService.get<string>('POSTGRES_DATABASE') || '',
