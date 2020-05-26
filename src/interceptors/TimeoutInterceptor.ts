930373f12c3182c0eb556c36ef46c98b02bfb968
Ale Sánchez <alesanchezmedina@gmail.com>
2020-05-26 13:57:55 +0200

    Removed unnecesary logging

--- src/interceptors/TimeoutInterceptor.ts
+++ src/interceptors/TimeoutInterceptor.ts
@@ -1,11 +1,10 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, RequestTimeoutException, Logger } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, RequestTimeoutException } from '@nestjs/common';
 import { Observable, throwError, TimeoutError } from 'rxjs';
 import { catchError, timeout } from 'rxjs/operators';
 
 @Injectable()
 export class TimeoutInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    Logger.log(`URL called: ${context.switchToHttp().getRequest().method} ${context.switchToHttp().getRequest().originalUrl}`);
     return next.handle().pipe(
       timeout(5000),
       catchError(err => {
