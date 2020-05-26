f08a348c061fe81a949cb19aad7c5b35d8b999dc
Ale Sánchez <alesanchezmedina@gmail.com>
2020-05-26 14:00:34 +0200

    Added global filters

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,25 @@ import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 import { TimeoutInterceptor } from './interceptors/TimeoutInterceptor';
 import { NATSConfigService } from './config/NATSConfigService';
+import { LoggingInterceptor } from './interceptors/LoggingInterceptor';
+import { AllExceptionsFilter } from './filters/ExceptionsFilter';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   const natsConfigService : NATSConfigService = app.get(NATSConfigService);
+  const configService : ConfigService = app.get<ConfigService>(ConfigService);
+
+  app.use(helmet());
+  app.useGlobalInterceptors(new TimeoutInterceptor(), new LoggingInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter);
 
   app.connectMicroservice({
     ...natsConfigService.getNATSConfig
   });
-
-  console.log(process.env);
-  app.use(helmet());
-  app.useGlobalInterceptors(new TimeoutInterceptor());
   app.startAllMicroservicesAsync();
-  await app.listen(3010);
+  await app.listen(configService.get<number>('PORT') || 3000, () => Logger.log('Microservice running'));
 }
 bootstrap();
